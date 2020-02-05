521ed3dd75bda9d4696d4cb97a970e91e08ad956
dtomeh <tomeh001@umn.edu>
2020-02-04 21:48:16 -0600

    Analysis done

--- Data and Code/week3.R
+++ Data and Code/week3.R
@@ -7,3 +7,12 @@ raw_df$timeStart <- as.POSIXct(as.Date(raw_df$timeStart, format = "%m/%d/%Y"))
 raw_df$timeEnd <- as.POSIXct(as.Date(raw_df$timeEnd, format = "%m/%d/%Y"))
 clean_df <- subset(raw_df, format(raw_df$timeStart, '%m') != '06')
 clean_df <- subset(clean_df, format(clean_df$q6) == "1")
+
+#Analysis 
+clean_df$timeSpent <- difftime(clean_df$timeStart, clean_df$timeEnd, units = c("secs"))
+hist((as.numeric(clean_df$timeSpent)))
+frequency_tables_list <- lapply(clean_df[, 5:14], function(x) {table(x)})
+lapply(frequency_tables_list, barplot)
+sum(clean_df$q1 >= clean_df$q2 & clean_df$q2 != clean_df$q3)
+for (table in frequency_tables_list) {
+  barplot(table)}
